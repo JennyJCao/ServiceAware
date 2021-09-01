98808ab2b3663a1a9a28fedd1e39d547ca9e6b40
JingCao <2410564208@qq.com>
2021-08-31 21:45:15 -0700

    update service status count in real time

--- internal/handlers/perform-checks.go
+++ internal/handlers/perform-checks.go
@@ -49,6 +49,20 @@ func (repo *DBRepo) ScheduledCheck(hostServiceID int) {
 	// tests the service
 	newStatus, msg := repo.testServiceForHost(h, hs)
 
+	// if the host service status has changed, broadcast to all clients
+	hostServiceStatusChanged := false
+	if newStatus != hs.Status {
+		hostServiceStatusChanged = true
+	}
+
+	if hostServiceStatusChanged {
+		data := make(map[string]string)
+		data["message"] = fmt.Sprintf("host service %s on %s has changed to %s", hs.Service.ServiceName, h.HostName, newStatus)
+		repo.broadcastMessage("public-channel", "host-service-status-changed", data)
+
+		// if appropriate, send email or SMS message
+	}
+
 	// update host service record in db with status (if changed) and
 	// update the last check
 	hs.Status = newStatus
@@ -59,11 +73,31 @@ func (repo *DBRepo) ScheduledCheck(hostServiceID int) {
 		return
 	}
 
-	// if the host service status has changed, broadcast to all clients
+	// broadcast the different count of services
+	if hostServiceStatusChanged {
+		pending, healthy, warning, problem, err := repo.DB.GetAllServiceStatusCounts()
+		if err != nil {
+			log.Println(err)
+			return
+		}
 
-	// if appropriate, send email or SMS message
+		data := make(map[string]string)
+		data["healthy_count"] = strconv.Itoa(healthy)
+		data["pending_count"] = strconv.Itoa(pending)
+		data["problem_count"] = strconv.Itoa(problem)
+		data["warning_count"] = strconv.Itoa(warning)
+		repo.broadcastMessage("public-channel", "host-service-count-changed", data)
+	}
+
 
 	log.Println("New status is", newStatus, "and msg is", msg)
+}
+
+func (repo *DBRepo) broadcastMessage(channel, messageType string, data map[string]string) {
+	err := app.WsClient.Trigger(channel, messageType, data)
+	if err != nil {
+		log.Println(err)
+	}
 }
 
 // TestCheck manually tests a host service and sends JSON response
