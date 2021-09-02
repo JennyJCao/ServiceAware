3479e4bfb99e5756f1a521ce3e5d74170c8f13fd
JingCao <2410564208@qq.com>
2021-09-01 19:09:49 -0700

    get the array of schedules

--- internal/handlers/schedule.go
+++ internal/handlers/schedule.go
@@ -1,12 +1,34 @@
 package handlers
 
 import (
+	"fmt"
 	"github.com/tsawler/vigilate/internal/helpers"
+	"github.com/tsawler/vigilate/internal/models"
+	"log"
 	"net/http"
 )
 
 // ListEntries lists schedule entries
 func (repo *DBRepo) ListEntries(w http.ResponseWriter, r *http.Request) {
+	var items []models.Schedule
+
+	for k, v := range repo.App.MonitorMap {
+		var item models.Schedule
+		item.ID = k
+		item.EntryID = v
+		item.Entry = app.Scheduler.Entry(v)
+		hs, err := repo.DB.GetHostServiceByID(k)
+		if err != nil {
+			log.Println(err)
+			return
+		}
+		item.ScheduleText = fmt.Sprintf("@every %d%s", hs.ScheduleNumber, hs.ScheduleUnit)
+		item.LastRunFromHS = hs.LastCheck
+		item.Host = hs.HostName
+		item.Service = hs.Service.ServiceName
+		items = append(items, item)
+	}
+
 	err := helpers.RenderPage(w, r, "schedule", nil, nil)
 	if err != nil {
 		printTemplateError(w, err)
